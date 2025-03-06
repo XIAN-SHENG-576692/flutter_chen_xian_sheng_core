eebc71a5011082edb53a04956a33a49ec7464151
XIAN-SHENG-576692 <chenxiansheng576692@gmail.com>
2025-03-06 23:50:04 +0800

    2025-03-06 23:50

--- lib/geometry/geometry_calculator.dart
+++ lib/geometry/geometry_calculator.dart
@@ -11,18 +11,18 @@ class GeometryCalculator {
   /// - [points]: The set of points to be mirrored.
   /// - [line]: The line relative to which the points are mirrored.
   /// Returns: An iterable containing the mirrored points.
-  static Iterable<Point<num>> calculateMirrorPoints({
-    required Iterable<Point<num>> points,
+  static Iterable<Point<double>> calculateMirrorPoints({
+    required Iterable<Point<double>> points,
     required Line line,
   }) {
-    final num a = line.start.y - line.end.y;
-    final num b = -(line.start.x - line.end.x);
-    final num c = line.start.x * line.end.y - line.end.x * line.start.y;
+    final double a = line.start.y - line.end.y;
+    final double b = -(line.start.x - line.end.x);
+    final double c = line.start.x * line.end.y - line.end.x * line.start.y;
 
-    final num denominator = a * a + b * b;
+    final double denominator = a * a + b * b;
 
     return points.map((point) {
-      final num factor = -2 * (a * point.x + b * point.y + c) / denominator;
+      final double factor = -2 * (a * point.x + b * point.y + c) / denominator;
       return Point(
         factor * a + point.x,
         factor * b + point.y,
@@ -35,15 +35,15 @@ class GeometryCalculator {
   /// - [points]: The collection of points to calculate cumulative positions for.
   /// Returns: A new collection of points where each point's position is the cumulative
   ///          sum relative to the previous points.
-  static Iterable<Point<T>> calculateCumulativePositions<T extends num>({
-    required Iterable<Point<T>> points,
+  static Iterable<Point<double>> calculateCumulativePositions({
+    required Iterable<Point<double>> points,
   }) sync* {
     if (points.isEmpty) return;
-    var cumulativePoint = Point<T>(0 as T, 0 as T);
+    var cumulativePoint = Point<double>(0, 0);
     for (final point in points) {
-      cumulativePoint = Point(
-        (cumulativePoint.x + point.x) as T,
-        (cumulativePoint.y + point.y) as T,
+      cumulativePoint = Point<double>(
+        (cumulativePoint.x + point.x),
+        (cumulativePoint.y + point.y),
       );
       yield cumulativePoint;
     }
